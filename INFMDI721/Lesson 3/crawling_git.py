2b73e8903d4a8ac2a09823718a3c3715b0a7ac7a
valentinphetchanpheng <vphetchanpheng@gmail.com>
2018-10-18 09:23:17 +0200

    modif présentation

--- INFMDI721/Lesson 3/crawling_git.py
+++ INFMDI721/Lesson 3/crawling_git.py
@@ -4,9 +4,8 @@ import pandas as pd
 import json
 
 url = "https://gist.github.com/paulmillr/2657075"
-token_code = '124c8fdeb9340fcdcdb651e987bb68dc9e1eb68f'
+token_code = '26ae353864868667f4d8e38074fca3d05e1b7139'
 head = {'Authorization': 'token {}'.format(token_code)}
-
 
 def get_list_user(url_page):
     page = requests.get(url_page)
@@ -14,22 +13,18 @@ def get_list_user(url_page):
     top_user = []
     for i in range(1,257):
         top_user.append(soup.find(text="#"+str(i)).parent.findNext('td').text)
-
     return top_user
 
 liste_user = get_list_user(url)
 
-
 def get_login(long_user):
     return long_user[:long_user.find('(')-1]
-
 
 def get_list_login(list_user):
     list_res = list(map(get_login, list_user))
     return list_res
 
 liste_login = get_list_login(liste_user)
-
 
 def get_list_mean_stars(list_login):
 
@@ -44,6 +39,7 @@ def get_list_mean_stars(list_login):
 
         sum_stars = 0
         mean_stars = 0
+
         for repo in json_repo:
             sum_stars += repo['stargazers_count']
 
@@ -55,15 +51,12 @@ def get_list_mean_stars(list_login):
         list_mean_stars.append(round(mean_stars,3))
 
     dico_user_mean_stars["Mean Stars"] = list_mean_stars
-
     return dico_user_mean_stars
 
 dico_liste_moyenne = get_list_mean_stars(liste_login)
-
 
 # Résultat : liste des moyennes des Stars rangées dans l'ordre décroissant dans DataFrame
 df_Stars = pd.DataFrame(dico_liste_moyenne)
 df_Stars_Sorted = df_Stars.sort_values(by='Mean Stars', ascending=False)
 print(df_Stars_Sorted)
-
 
